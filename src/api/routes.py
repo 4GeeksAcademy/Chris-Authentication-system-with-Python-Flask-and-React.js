fdce262307fd694e953247793aa327534a9be271
ChrisKeith01 <165314148+ChrisKeith01@users.noreply.github.com>
2025-03-05 16:30:16 +0000

    Route started

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,20 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    email=request.json.get("email", None)
+    password=request.json.get("password", None)
+    if email is None or password is None:
+        return jsonify({"msg" : "password and email required"}), 400
+    if User.query.filter_by(email=email).first():
+        return jsonify({"msg" : "this email is already taken"}), 400
+    
+    hashed_password=generate_password_hash(password)
+    new_user=User(email=email, password=hashed_password, is_active=True)
+
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"msg" : "user created succesfully"}), 201 
